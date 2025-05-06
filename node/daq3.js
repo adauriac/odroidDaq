1f967ea2462977dc82628fe0b592a00307fb8442
Anglès d'Auriac JC <jeanchristian@anglesdauriac.fr>
2025-05-06 14:10:55 +0200

    afichage du flag JCFFT

--- node/daq3.js
+++ node/daq3.js
@@ -2,8 +2,7 @@
 // liaison avec le module DAQ3 via la liaison serie
 const daq3Version = '20221103';
 const JC=1
-if (JC==1)
-    console.log("flag JC set ")
+console.log("flag JC set ",JC)
 const serialport = require("serialport");
 var sp, SerialPort = serialport.SerialPort;
 var events = require('events');
