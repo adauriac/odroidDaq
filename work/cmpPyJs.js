46df9fef8f9b011a79e27f489e5417281715c28e
dauriac <jeanchristian@anglesdauriac.fr>
2025-05-21 19:56:51 +0000

    window and fft called, NEED TO NORMALIZE the output

--- work/cmpPyJs.js
+++ work/cmpPyJs.js
@@ -10,7 +10,7 @@ use: github.com/indutny/fft.js/blob/master/README.md.
 
 const fs = require('fs')
 
-if (process.argv.length!=3) {
+if (process.argv.length < 3) {
     console.log("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
     process.exit(0)
 }
@@ -22,20 +22,44 @@ if (quoi!='f' && quoi!='w'){
 const contenu = fs.readFileSync("./tmp",'utf8')
 const lines = contenu.split(/\r?\n/)
 lines.pop()
-const signal = lines.map(ligne=>parseFloat(ligne,10))
+const data = lines.map(ligne=>parseFloat(ligne,10))
+let N = data.length
 if (quoi=='f') {
     const FFT = require('./lib/fft.js') // JC ?????????????????
-    N = signal.length
     const fft = new FFT(N);
     let fftOut = fft.createComplexArray();
-    fft.realTransform(fftOut, signal);
+    fft.realTransform(fftOut, data);
     fft.completeSpectrum(fftOut);
 
     for(let i=0;i<N;i++) {
 	let re = fftOut[2*i]
 	let im = fftOut[2*i+1]
-	console.log(`${i} ${signal[i]} ${re} ${im}`)
+	console.log(`${i} ${data[i]} ${re} ${im}`)
     }
 } else {
     console.log("in progress")
+    let freqSampling = 10
+    // the frequencies are i*freqSampling/len(data) 0<=i<=len(data)//2
+    const f = Array.from({ length: N/2 + 1 }, (_, i) => i*freqSampling/N);
+    // apply hann windows to the data
+    for (let i=0;i<N;i++) {
+	w = 0.5 - 0.5*Math.cos(2*Math.PI*i/N)
+	data[i] *= w
+    }
+    const FFT = require('./lib/fft.js') // JC ?????????????????
+    const fft = new FFT(N);
+    let fftOut = fft.createComplexArray();
+    fft.realTransform(fftOut, data);
+    fft.completeSpectrum(fftOut);
+
+    for(let i=0;i<N;i++) {
+	let re = fftOut[2*i]
+	let im = fftOut[2*i+1]
+	console.log(`${i} ${data[i]} ${re} ${im}`)
+    }
+
+    console.log("# welch en pythonjavascript freqSampling=${freqSampling}")
+    for (let i=0;i<=N/2;i++) {
+	console.log(i,f[i])
+    }
 }
--- work/cmpPyJs.py
+++ work/cmpPyJs.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import numpy as np
 import sys
 from scipy import fft
+from scipy import signal
 """
 pour comparer python et js : cmpPyJs.js
 On calcule la fft simple de python, et on fera de meme dans cmpPyJs.js
@@ -9,7 +11,7 @@ Sort sur stdout i s[i] Re(fft(i)) Im(fft(i] abs(fft[i])
 """
 # Paramètres du signal
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
     exit(0)
 if sys.argv[1]!='f' and sys.argv[1]!='w':
@@ -17,16 +19,26 @@ if sys.argv[1]!='f' and sys.argv[1]!='w':
     exit(0)
 
 lines = open("tmp").readlines()
-signal = np.array(list(map(float,lines)))
-N = len(signal)
+data = np.array(list(map(float,lines)))
+N = len(data)
 if sys.argv[1]=='f':
     # FFT du signal
-    X = fft(signal)
+    X = fft(data)
     # Calcul de la densité spectrale de puissance (DSP)
     for i in range(N):
-        print(f"{i} {signal[i]} {X[i].real} {X[i].imag}")# {np.sqrt(abs(X[i]))}")
+        print(f"{i} {data[i]} {X[i].real} {X[i].imag}")# {np.sqrt(abs(X[i]))}")
 else:
-    print("in progress")
+    nbperseg = len(data)
+    freqSampling = 10
+    f, P = signal.welch(data,
+                        freqSampling,
+                        'hann',
+                        nperseg=nbperseg,
+                        scaling='density')
+    assert len(f)==len(P)
+    print(f"# welch en python {freqSampling=}")
+    for i in range(len(P)):
+        print(i,f[i],P[i])
     exit(0)
 
         
