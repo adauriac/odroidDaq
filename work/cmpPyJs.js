8d1e22c8fa787dcd7b203054198b080d76c2ec98
Your Name <dauriac@neel.cnrs.fr>
2025-05-21 08:03:41 +0200

    welch.lyx corrige

--- work/cmpPyJs.js
+++ work/cmpPyJs.js
@@ -1,24 +1,21 @@
 /* 
 pour comparer python et js : cmpPyJs.js
 On calcule la fft simple de python, et on fera de meme dans cmpPyJs.js
-On donne le signal comme une formule 
+On donne le signal dans le fichier nommé tmp
 Sort sur stdout i s[i] Re(fft(i)) Im(fft(i] abs(fft[i])
 
-node -i cmpJsPy.js
+appel: node cmpJsPy.js
 use: github.com/indutny/fft.js/blob/master/README.md.
 */
-var FFT = require('./lib/fft.js') // JC ?????????????????
 
-function f(t) {return Math.sin(2 * Math.PI * 100 * t)}
+const fs = require('fs')
+const contenu = fs.readFileSync("./tmp",'utf8')
+const lines = contenu.split(/\r?\n/)
+lines.pop()
+const signal = lines.map(ligne=>parseFloat(ligne,10))
 
-// Paramètres du signal
-let fs = 1024                // Fréquence d'échantillonnage en Hz
-let T = 1.0                  // Durée en secondes
-let N = Math.round(T * fs)          // Nombre d'échantillons
-//t = np.linspace(0, T, N, endpoint=False)
-// console.log(N)
-const Ts = Array.from({ length: N }, (_, i) => i/N);
-const signal = Ts.map(f)
+const FFT = require('./lib/fft.js') // JC ?????????????????
+N = signal.length
 const fft = new FFT(N);
 let fftOut = fft.createComplexArray();
 fft.realTransform(fftOut, signal);
--- work/cmpPyJs.py
+++ work/cmpPyJs.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.fft import fft
+from scipy import fft
 """
 pour comparer python et js : cmpPyJs.js
 On calcule la fft simple de python, et on fera de meme dans cmpPyJs.js
@@ -7,17 +7,13 @@ On donne le signal comme une formule
 Sort sur stdout i s[i] Re(fft(i)) Im(fft(i] abs(fft[i])
 """
 # Paramètres du signal
-fs = 1024                # Fréquence d'échantillonnage en Hz
-T = 1.0                  # Durée en secondes
-N = int(T * fs)          # Nombre d'échantillons
-t = np.linspace(0, T, N, endpoint=False)
 
-# Signal : sinus de 100 Hz + un peu de bruit
-signal = np.sin(2 * np.pi * 100 * t) 
-
+lines = open("tmp").readlines()
+signal = np.array(list(map(float,lines)))
+N = len(signal)
 # FFT du signal
 X = fft(signal)
 
 # Calcul de la densité spectrale de puissance (DSP)
 for i in range(N):
-    print(f"{i} {signal[i]} {X[i].real} {X[i].imag} {np.sqrt(abs(X[i]))}")
+    print(f"{i} {signal[i]} {X[i].real} {X[i].imag}")# {np.sqrt(abs(X[i]))}")
