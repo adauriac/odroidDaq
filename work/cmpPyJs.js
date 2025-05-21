6a95c677ff1239161f280a36804df98cb5529257
dauriac <jeanchristian@anglesdauriac.fr>
2025-05-21 07:32:56 +0000

    preparation of welch comparison, not only fft, in cmpPyJs

--- work/cmpPyJs.js
+++ work/cmpPyJs.js
@@ -9,20 +9,33 @@ use: github.com/indutny/fft.js/blob/master/README.md.
 */
 
 const fs = require('fs')
+
+if (process.argv.length!=3) {
+    console.log("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
+    process.exit(0)
+}
+let quoi = process.argv[2];
+if (quoi!='f' && quoi!='w'){
+    console.log("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
+    process.exit(0)
+}
 const contenu = fs.readFileSync("./tmp",'utf8')
 const lines = contenu.split(/\r?\n/)
 lines.pop()
 const signal = lines.map(ligne=>parseFloat(ligne,10))
+if (quoi=='f') {
+    const FFT = require('./lib/fft.js') // JC ?????????????????
+    N = signal.length
+    const fft = new FFT(N);
+    let fftOut = fft.createComplexArray();
+    fft.realTransform(fftOut, signal);
+    fft.completeSpectrum(fftOut);
 
-const FFT = require('./lib/fft.js') // JC ?????????????????
-N = signal.length
-const fft = new FFT(N);
-let fftOut = fft.createComplexArray();
-fft.realTransform(fftOut, signal);
-fft.completeSpectrum(fftOut);
-
-for(let i=0;i<N;i++) {
-    let re = fftOut[2*i]
-    let im = fftOut[2*i+1]
-    console.log(`${i} ${signal[i]} ${re} ${im}`)
+    for(let i=0;i<N;i++) {
+	let re = fftOut[2*i]
+	let im = fftOut[2*i+1]
+	console.log(`${i} ${signal[i]} ${re} ${im}`)
+    }
+} else {
+    console.log("in progress")
 }
--- work/cmpPyJs.py
+++ work/cmpPyJs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from scipy import fft
 """
 pour comparer python et js : cmpPyJs.js
@@ -8,12 +9,24 @@ Sort sur stdout i s[i] Re(fft(i)) Im(fft(i] abs(fft[i])
 """
 # Paramètres du signal
 
+if len(sys.argv) != 2:
+    print("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
+    exit(0)
+if sys.argv[1]!='f' and sys.argv[1]!='w':
+    print("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
+    exit(0)
+
 lines = open("tmp").readlines()
 signal = np.array(list(map(float,lines)))
 N = len(signal)
-# FFT du signal
-X = fft(signal)
+if sys.argv[1]=='f':
+    # FFT du signal
+    X = fft(signal)
+    # Calcul de la densité spectrale de puissance (DSP)
+    for i in range(N):
+        print(f"{i} {signal[i]} {X[i].real} {X[i].imag}")# {np.sqrt(abs(X[i]))}")
+else:
+    print("in progress")
+    exit(0)
 
-# Calcul de la densité spectrale de puissance (DSP)
-for i in range(N):
-    print(f"{i} {signal[i]} {X[i].real} {X[i].imag}")# {np.sqrt(abs(X[i]))}")
+        
