066b962257bd804ad795b0cbd3654b2179db343d
Your Name <dauriac@neel.cnrs.fr>
2025-05-22 18:43:42 +0200

    better output for cmpJsPy.sh

--- work/cmpPyJs.js
+++ work/cmpPyJs.js
@@ -10,8 +10,38 @@ use: github.com/indutny/fft.js/blob/master/README.md.
 
 const fs = require('fs')
 
+function welchise(data,freqSampling,n) {
+    let N = data.length
+    const f = Array.from({ length: N/2 + 1 }, (_, i) => i*freqSampling/N);
+    let U = 0
+    for (let i=0;i<N;i++) {
+	let w = 0.5 - 0.5*Math.cos(2*Math.PI*i/N)
+	U += w*w
+	data[i] *= w
+    }
+    const FFT = require('./lib/fft.js') 
+    const fft = new FFT(N)
+    const c = 1/(freqSampling*U)
+    let fftOut = fft.createComplexArray();
+    fft.realTransform(fftOut, data);
+    fft.completeSpectrum(fftOut);
+    P = new Array(N/2+1)
+    for(let i=0;i<=N/2;i++) {
+	let re = fftOut[2*i]
+	let im = fftOut[2*i+1]
+	P[i] = c*(re*re+im*im)
+	if (i!=N/2)
+	    P[i] *= 2
+    }
+    // console.log(`# welch en javascript freqSampling=${freqSampling} ${N}`)
+    // for (let i=0;i<=N/2;i++) 
+    // 	console.log(i,f[i],P[i])
+    return [f,P]
+} // function welchise(data,freqSampling,n) {
+// ***************************************************************************************
+
 if (process.argv.length < 3) {
-    console.log("syntax: node cmpPyJs.js f/w     (pour welch ou fft)")
+    console.log("syntax: node cmpPyJs.js f/w  freqSampling   (pour welch ou fft)")
     process.exit(0)
 }
 let quoi = process.argv[2];
@@ -39,40 +69,8 @@ if (quoi=='f') {
     }
 } else {
     let freqSampling = parseFloat(process.argv[3])
-    // the frequencies are i*freqSampling/len(data) 0<=i<=len(data)//2
-    const f = Array.from({ length: N/2 + 1 }, (_, i) => i*freqSampling/N);
-
-    // apply hann windows to the data
-    let U = 0
-    for (let i=0;i<N;i++) {
-	let w = 0.5 - 0.5*Math.cos(2*Math.PI*i/N)
-	U += w*w
-	data[i] *= w
-    }
-    
-    const FFT = require('./lib/fft.js') // JC ?????????????????
-    const fft = new FFT(N)
-    const c = 1/(freqSampling*U)
-    let fftOut = fft.createComplexArray();
-    fft.realTransform(fftOut, data);
-    fft.completeSpectrum(fftOut);
-    P = new Array(N/2+1)
-    for(let i=0;i<=N/2;i++) {
-	let re = fftOut[2*i]
-	let im = fftOut[2*i+1]
-	P[i] = c*(re*re+im*im)
-	if (i!=N/2)
-	    P[i] *= 2
-    }
-    if (false) {
-	for(let i=0;i<N;i++) {
-	    let re = fftOut[2*i]
-	    let im = fftOut[2*i+1]
-	    console.log(`${i} ${data[i]} ${re} ${im} ${P[i]}`)
-	}
-    }
-
-    console.log(`# welch en javascript freqSampling=${freqSampling}`)
+    let [f,P] = welchise(data,freqSampling,1)
+    console.log(`# welch en javascript freqSampling=${freqSampling} ${N}`)
     for (let i=0;i<=N/2;i++) {
 	console.log(i,f[i],P[i])
     }
--- work/cmpPyJs.sh
+++ work/cmpPyJs.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # node cmpPyJs.js
-echo "genrating signal in the fileexampleSignal"
-python3 makeSignal.py 9
+if [ "$#" -ne 3 ]; then
+    echo "syntax: cmpPyJs.sh sizeOfData [f/w] freqSampling "
+    echo "create a random signal of size 2**sizeOfData and perform a welch or fft xform."
+    echo "for the welch case the last argument is the frequency sampling"
+    exit 1
+fi
+
+echo "generating signal in the file exampleSignal"
+python3 makeSignal.py $1
 
 echo "performing transformation"
-cmdj="node cmpPyJs.js $1 $2"
-cmdp="python3 cmpPyJs.py $1 $2"
+cmdj="node cmpPyJs.js $2 $3"
+cmdp="python3 cmpPyJs.py $2 $3"
 $cmdj | tee tmpjs
 $cmdp | tee tmppy
 echo ""
 echo ""
 echo "# Resume"
 echo "# frequence javascript python (les freq sont bien les memes)"
-if [ "$1" = "w" ]; then
-    paste tmpjs tmppy | grep -v \# | gawk '{if (($1!=$4) || ($2!=$5)) print("MERDE"); else printf("%lf %lf %lf\n",$2,$3,$6)}'
+if [ "$2" = "w" ]; then
+    paste tmpjs tmppy | grep -v \# | gawk '{if (($1!=$4) || ($2!=$5)) print("MERDE"); else printf("%15.10lf %15.10lf %15.10lf  %s\n",$2,$3,$6,($3-$6)**2>1e-12 ? "*": " ")}'
 else
     paste tmpjs tmppy | grep -v \# | gawk '{if (($1!=$5) || ($2!=$6)|| ($3!=$7) || ($4!=$8)) print($0);}'
 fi
